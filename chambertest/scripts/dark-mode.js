f3223c06f742124bfa1c2e46de5c3fcfedfec36e
nameerfdt <nameerfdt@byui.edu>
2024-02-05 20:07:06 -0800

    dark mode grid box testing

--- chambertest/scripts/dark-mode.js
+++ chambertest/scripts/dark-mode.js
@@ -1,8 +1,10 @@
 const modeButton = document.querySelector("#dark-mode");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
+const gridbox = document.querySelector(".grid-box");
 
 modeButton.addEventListener("click", () => {
     body.classList.toggle("dark-mode");
     main.classList.toggle("dark-mode");
+    gridbox.classList.toggle("dark-mode");
 });
