9bca40f5a21c2e75a336bcfc9f9fbf45274f2922
nameerfdt <nameerfdt@byui.edu>
2024-02-07 17:35:39 -0800

    updates

--- darkmodetest/darkmode.js
+++ darkmodetest/darkmode.js
@@ -1,11 +1,15 @@
 const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
-const section = document.querySelector("section");
+// const section = main.children;
+const grid1 = document.querySelector('#grid1');
+const grid2 = document.querySelector('#grid2');
 
 
 modeButton.addEventListener("click", () => {
 	body.classList.toggle('dark-mode');
 	main.classList.toggle('dark-mode');
-    section.classList.toggle('dark-mode');
+    // section.classList.toggle('dark-mode');
+    grid1.classList.toggle('dark-mode');
+    grid2.classList.toggle('dark-mode');
 });
