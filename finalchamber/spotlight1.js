faa843c519c69c1766684517b9b41e9346a3fe98
nameerfdt <nameerfdt@byui.edu>
2024-03-17 21:22:27 -0700

    bronze members

--- finalchamber/spotlight1.js
+++ finalchamber/spotlight1.js
@@ -19,7 +19,7 @@ async function getMemberData() {
            <p>${selectedGoldMember.description}</p>
            <p>Address: ${selectedGoldMember.address}, ${selectedGoldMember.city_state}</p>
            <p>Phone: ${selectedGoldMember.phone}</p>
-           <p><a href="${selectedGoldMember.website}" target="_blank">Website</a></p>
+           <p><a href="${selectedGoldMember.website}" target="_blank">${selectedGoldMember.website}</a></p>
 
        `;
    }
@@ -28,8 +28,8 @@ async function getMemberData() {
     const silverMembers = data.members.filter(member => member.membership_level === 'Silver');
 
     // Randomly select a silver member
-    const randomIndex = Math.floor(Math.random() * silverMembers.length);
-    const selectedSilverMember = silverMembers[randomIndex];
+    const randomSilverIndex = Math.floor(Math.random() * silverMembers.length);
+    const selectedSilverMember = silverMembers[randomSilverIndex];
 
     // Update HTML div with selected silver member's information
     const silverMemberDiv = document.getElementById("silverMember");
@@ -39,9 +39,28 @@ async function getMemberData() {
             <p>${selectedSilverMember.description}</p>
             <p>Address: ${selectedSilverMember.address}, ${selectedSilverMember.city_state}</p>
             <p>Phone: ${selectedSilverMember.phone}</p>
-            <p><a href="${selectedSilverMember.website}" target="_blank">Website</a></p>
+            <p><a href="${selectedSilverMember.website}" target="_blank">${selectedSilverMember.website}</a></p>
         `;
     }
+
+        // Filter bronze members
+        const brozeMembers = data.members.filter(member => member.membership_level === 'Silver');
+
+        // Randomly select a silver member
+        const randomBronzeIndex = Math.floor(Math.random() * silverMembers.length);
+        const selectedBronzeMember = silverMembers[randomBronzeIndex];
+    
+        // Update HTML div with selected silver member's information
+        const bronzeMemberDiv = document.getElementById("bronzeMember");
+        if (selectedBronzeMember) {
+            bronzeMemberDiv.innerHTML = `
+                <h2>${selectedBronzeMember.name}</h2>
+                <p>${selectedBronzeMember.description}</p>
+                <p>Address: ${selectedBronzeMember.address}, ${selectedBronzeMember.city_state}</p>
+                <p>Phone: ${selectedBronzeMember.phone}</p>
+                <p><a href="${selectedBronzeMember.website}" target="_blank">${selectedBronzeMember.website}</a></p>
+            `;
+        }
 }
 
 getMemberData();
